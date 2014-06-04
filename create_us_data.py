efb3002cff8bb382f9aecf6e6c39f2b1b67b355c
bsweger <bsweger@gmail.com>
2014-06-04 14:33:56 -0400

    add food security data to the acs indicators - one file to rule them all

--- create_us_data.py
+++ create_us_data.py
@@ -2,10 +2,17 @@
 ## Create U.S. food security and indicators file for analysis
 ## Since this is meant to be simple data for practicing visualizations,
 ## completely ignoring margins of error.
+## Years represent 3-year estimates (e.g., 2011 represents 2009-2011)
 ##
 
-import pandas as pd
+import pandas as pd, numpy as np
 from decimal import Decimal
+
+def get_fips(x):
+    if x[-2:] <> 'US':
+        return x[-2:]
+    else:
+        return '00'
 
 def household_snap_rate(row):
     return round(
@@ -13,7 +20,12 @@ def household_snap_rate(row):
         ,2)
 
 #food security numbers from CPS Food Security Supplement (3 year estimates)
-fs = pd.read_csv('data/usda_cps_food_security.csv')
+fs = pd.read_csv(
+    'data/usda_cps_food_security.csv',
+    dtype={'fips':np.object}
+)
+
+fs.set_index(['fips','year'],inplace=True)
 
 #household SNAP totals from 3-year ACS estimates
 acscols = ['GEO.id','HC01_EST_VC01','HC02_EST_VC01']
@@ -54,13 +66,22 @@ dp = pd.concat([dp_2008, dp_2009, dp_2010, dp_2011, dp_2012])
 dp = dp[dp.fips != 'Id']
 dp.set_index(['fips','year'],inplace=True)
 
-#merge into one dataframe, the split up by US and states, and save
+#merge into one dataframe, the split up by US and states
+#(setting & resetting the multi-index = more convenient concat/subset syntax)
 us = pd.concat([snap,dp],axis=1)
 us = us.reset_index()
-us['fips'] = us['fips'].map(lambda x: x[-2:])
-states = us[us['fips'] <> 'US']
-us = us[us['fips'] == 'US']
-#states = us.iloc[us.index.get_level_values('geoid') <> '0100000US']
-#us = us.iloc[us.index.get_level_values('geoid') == '0100000US']
-states.to_csv('data/us_state_indicators.csv')
-us.to_csv('data/us_indicators.csv')
+us['fips'] = us['fips'].apply(lambda x: get_fips(x))
+us.set_index(['fips','year'],inplace=True)
+us = pd.concat([us, fs], axis=1)
+us = us.reset_index()
+states = us[us['fips'] <> '00']
+us = us[us['fips'] == '00']
+
+#save US and state files (selected columns)
+collist = [
+    'state_name', 'year', 'food_insecure_percent',
+    'food_insecure_low_percent', 'household_snap_rate',
+    'unemployment_rate', 'poverty_rate', 'state_abbr', 'fips'
+]
+states.to_csv('data/us_state_indicators.csv', cols=collist, index=False)
+us.to_csv('data/us_indicators.csv', cols=collist, index=False)
