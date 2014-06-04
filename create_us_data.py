46a2e6377e155d5589c299071fdd1f8781515710
bsweger <bsweger@gmail.com>
2014-06-04 13:48:34 -0400

    replace ACS geoid with 2-digit fips

--- create_us_data.py
+++ create_us_data.py
@@ -16,46 +16,51 @@ def household_snap_rate(row):
 fs = pd.read_csv('data/usda_cps_food_security.csv')
 
 #household SNAP totals from 3-year ACS estimates
-snapcols = ['GEO.id','HC01_EST_VC01','HC02_EST_VC01']
-snap_2008 = pd.read_csv('data/ACS_08_3YR_S2201_with_ann.csv', usecols=snapcols)
+acscols = ['GEO.id','HC01_EST_VC01','HC02_EST_VC01']
+snap_2008 = pd.read_csv('data/ACS_08_3YR_S2201_with_ann.csv', usecols=acscols)
 snap_2008['year'] = 2008
-snap_2009 = pd.read_csv('data/ACS_09_3YR_S2201_with_ann.csv', usecols=snapcols)
+snap_2009 = pd.read_csv('data/ACS_09_3YR_S2201_with_ann.csv', usecols=acscols)
 snap_2009['year'] = 2009
-snap_2010 = pd.read_csv('data/ACS_10_3YR_S2201_with_ann.csv', usecols=snapcols)
+snap_2010 = pd.read_csv('data/ACS_10_3YR_S2201_with_ann.csv', usecols=acscols)
 snap_2010['year'] = 2010
-snap_2011 = pd.read_csv('data/ACS_11_3YR_S2201_with_ann.csv', usecols=snapcols)
+snap_2011 = pd.read_csv('data/ACS_11_3YR_S2201_with_ann.csv', usecols=acscols)
 snap_2011['year'] = 2011
-snap_2012 = pd.read_csv('data/ACS_12_3YR_S2201_with_ann.csv', usecols=snapcols)
+snap_2012 = pd.read_csv('data/ACS_12_3YR_S2201_with_ann.csv', usecols=acscols)
 snap_2012['year'] = 2012
 snap = pd.concat([snap_2008, snap_2009, snap_2010, snap_2011, snap_2012])
-snap.columns = ['geoid', 'households_total', 'households_snap', 'year']
-snap = snap[snap.geoid != 'Id']
+snap.columns = ['fips', 'households_total', 'households_snap', 'year']
+snap = snap[snap.fips != 'Id']
 snap['household_snap_rate'] = snap.apply(lambda row: household_snap_rate(row), axis=1)
-snap.set_index(['geoid','year'],inplace=True)
+snap.set_index(['fips','year'],inplace=True)
 
 #selected economic profiles from 3-year ACS estimates
+dpcols = ['fips', 'unemployment_rate', 'poverty_rate', 'year']
 dp_2008 = pd.read_csv('data/ACS_08_3YR_DP03_with_ann.csv', usecols=['GEO.id', 'HC01_EST_VC10', 'HC01_EST_VC103'])
 dp_2008['year'] = 2008
-dp_2008.columns = ['geoid', 'unemployment_rate', 'poverty_rate', 'year']
+dp_2008.columns = dpcols
 dp_2009 = pd.read_csv('data/ACS_09_3YR_DP03_with_ann.csv', usecols=['GEO.id', 'HC01_EST_VC10', 'HC01_EST_VC111'])
 dp_2009['year'] = 2009
-dp_2009.columns = ['geoid', 'unemployment_rate', 'poverty_rate', 'year']
+dp_2009.columns = dpcols
 dp_2010 = pd.read_csv('data/ACS_10_3YR_DP03_with_ann.csv', usecols=['GEO.id', 'HC03_VC13', 'HC03_VC156'])
 dp_2010['year'] = 2010
-dp_2010.columns = ['geoid', 'unemployment_rate', 'poverty_rate', 'year']
+dp_2010.columns = dpcols
 dp_2011 = pd.read_csv('data/ACS_11_3YR_DP03_with_ann.csv', usecols=['GEO.id', 'HC03_VC13', 'HC03_VC156'])
 dp_2011['year'] = 2011
-dp_2011.columns = ['geoid', 'unemployment_rate', 'poverty_rate', 'year']
+dp_2011.columns = dpcols
 dp_2012 = pd.read_csv('data/ACS_12_3YR_DP03_with_ann.csv', usecols=['GEO.id', 'HC03_VC13', 'HC03_VC156'])
 dp_2012['year'] = 2012
-dp_2012.columns = ['geoid', 'unemployment_rate', 'poverty_rate', 'year']
+dp_2012.columns = dpcols
 dp = pd.concat([dp_2008, dp_2009, dp_2010, dp_2011, dp_2012])
-dp = dp[dp.geoid != 'Id']
-dp.set_index(['geoid','year'],inplace=True)
+dp = dp[dp.fips != 'Id']
+dp.set_index(['fips','year'],inplace=True)
 
 #merge into one dataframe, the split up by US and states, and save
 us = pd.concat([snap,dp],axis=1)
-states = us.iloc[us.index.get_level_values('geoid') <> '0100000US']
-us = us.iloc[us.index.get_level_values('geoid') == '0100000US']
+us = us.reset_index()
+us['fips'] = us['fips'].map(lambda x: x[-2:])
+states = us[us['fips'] <> 'US']
+us = us[us['fips'] == 'US']
+#states = us.iloc[us.index.get_level_values('geoid') <> '0100000US']
+#us = us.iloc[us.index.get_level_values('geoid') == '0100000US']
 states.to_csv('data/us_state_indicators.csv')
 us.to_csv('data/us_indicators.csv')
